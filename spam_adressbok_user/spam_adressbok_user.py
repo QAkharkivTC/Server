61407fb9696c44f44f9bc976d7493fa2c1ffc51b
QAkharkivTC <88654511+QAkharkivTC@users.noreply.github.com>
2021-08-09 12:45:09 +0300

    Update spam_adressbok_user.py

--- spam_adressbok_user/spam_adressbok_user.py
+++ spam_adressbok_user/spam_adressbok_user.py
@@ -10,9 +10,9 @@ import ssl
 def spam_add_contacts(N):
     ssl._create_default_https_context = ssl._create_unverified_context
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    localhost = 'qa3.trueconf.net'
-    user = 'artem'
-    token = '00e07cc6fc3ddb69c07facb05e242653df9592a1'
+    localhost = 'qa3.trueconf.net'                                          # указать ip сервера
+    user = 'artem'                                                          # указать id пользователя
+    token = '0000000000000000000000000000'                                  # указать токен сервера
     for i in range(N):
         data = {"contact_id":"SPAM_"+str(i)+"","display_name":"SPAM_"+str(i)+""}
         r = requests.post('https://'+localhost+'/api/v3.1/users/'+user+'/addressbook?access_token='+token+'', data=data , verify=False)
